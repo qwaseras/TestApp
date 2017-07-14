440939106797807987e0c3cdfd54c90ab40671c8
qwaseras <qwqwqaqs@gmail.com>
2017-07-14 17:51:48 +0300

    deleted app controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def show
+
+  end
 
    def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Succesfully registrated!"
       redirect_to @user
     else
       render 'new'
